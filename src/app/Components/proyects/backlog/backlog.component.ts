182198e9d5c4bba4759b1be1fa014a7aa4a6dc30
Jeisson Hernandez <jeissonhrdz186@gmail.com>
2024-06-28 11:15:58 -0500

    UI: Actualizar backlog y task
    Se cambio la forma como se ve el backlog, ya no en una modal, y se creo la modal para crear una nueva tarea}

--- src/app/Components/proyects/backlog/backlog.component.ts
+++ src/app/Components/proyects/backlog/backlog.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AddTaskComponent } from "./add-task/add-task.component";
 
 @Component({
-  selector: 'app-backlog',
-  standalone: true,
-  imports: [],
-  templateUrl: './backlog.component.html',
-  styleUrl: './backlog.component.css'
+    selector: 'app-backlog',
+    standalone: true,
+    templateUrl: './backlog.component.html',
+    styleUrl: './backlog.component.css',
+    imports: [AddTaskComponent]
 })
 export class BacklogComponent {
 
